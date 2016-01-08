1170c15ca671b4cfcc2eb8112992a85ee80d2566
Chris Dias <chris@diasfam.com>
2016-01-08 15:06:37 -0800

    add jsx test

---
+++ test/test.jsx
@@ -0,0 +1,15 @@
+// Awkward block of variable declarations
+var Form      = MyFormComponent;  
+var FormRow   = Form.Row;  
+var FormLabel = Form.Label;  
+var FormInput = Form.Input;  
+
+var App = (
+  <Form>
+    <FormRow>
+      <FormLabel />
+      <FormInput />
+    </FormRow>
+  </Form>
+);
+
